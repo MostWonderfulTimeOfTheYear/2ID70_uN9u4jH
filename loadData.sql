3424130b23d9d4fff51db181ee9149fd75e899ea
MostWonderfulTimeOfTheYear <isabelle.cooijmans@gmail.com>
2019-03-06 22:23:28 +0100

    Update loadData.sql

--- loadData.sql
+++ loadData.sql
@@ -1,15 +1,3 @@
--- this file should load all data in the previously-created tables
--- the data will be stored under /mnt/ramdisk/tables
--- for example, the Students file is under /mnt/ramdisk/tables/Students.table 
--- The files of the folder are as follows (mind the lower-case/upper-case): 
---   CourseOffers.table, CourseRegistrations.table, Courses.table, Degrees.table
---   StudentAssistants.table, StudentRegistrationsToDegrees.table, Students.table
---   TeacherAssignmentsToCourses.table, Teachers.table
--- Don't forget to analyze at the end. It can make a difference in query performance.
--- Example:
-
--- 
-
 COPY Students(StudentId, StudentName, Address, BirthyearStudent, Gender) FROM '/mnt/ramdisk/tables/students.table' DELIMITER ',' CSV HEADER;
 COPY Degrees(DegreeId, Dept, DegreeDescription, TotalECTS) FROM '/mnt/ramdisk/tables/degrees.table' DELIMITER ',' CSV HEADER;
 COPY StudentRegistrationsToDegrees(StudentRegistrationId, StudentId, DegreeId, RegistrationYear) FROM '/mnt/ramdisk/tables/StudentRegistrationsToDegrees.table' DELIMITER ',' CSV HEADER; 
