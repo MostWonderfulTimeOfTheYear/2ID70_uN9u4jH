2ade519f3eb2ac6186783633d798d0f5eba4eff2
MostWonderfulTimeOfTheYear <isabelle.cooijmans@gmail.com>
2019-03-06 22:22:18 +0100

    Update load.sh

--- load.sh
+++ load.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
-# This script will create the tables and load all data. A sample template follows, but feel free to merge the two files.
-# The script will be given 20 minutes and killed afterwards
-# Expected size of tables after loading should be below 8 GB
-# The total disk space available for storing all data, views, indices, and the write-ahead-log (also see UNLOGGED keyword) is 12 GB
-
-# remove '-U=postgres' before submitting
-
-psql -d uni -f createTables.sql -U postgres
-psql -d uni -f loadData2.sql -U postgres
+psql -d uni -f createTables.sql 
+psql -d uni -f loadData.sql 
